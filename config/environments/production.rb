ccee025f9f637c73b9f67179e9ac4efefa9f4e53
angelopoole <angelo.poole@outlook.com>
2020-02-28 10:18:03 -0500

    added jws cors and serializers, removed: test suite;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "back_leagueveiws_api_production"
+  # config.active_job.queue_name_prefix = "callinIt_production"
 
   config.action_mailer.perform_caching = false
 
